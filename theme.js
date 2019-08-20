4e0fd673d97e179c066394ef5383cf067bf2e88c
Zachary <53413395+ze-ck@users.noreply.github.com>
2019-08-19 21:22:26 -0400

    Add files via upload
    First implementation of personally-coded JavaScript features
    - Added the ability to change between light and dark themes for the entire site.
    
    Will work on having the website stay on the chosen theme when moving to different pages.

---
+++ theme.js
@@ -0,0 +1,54 @@
+var theme = document.querySelector("img");
+var jumbo = document.querySelector(".jumbotron");
+var navbar = document.querySelector(".tentaclenav");
+var body = document.querySelector("body");
+var navbarTheme = document.querySelector(".navbar-light");
+var dropdown = document.querySelectorAll(".tentacledrop");
+var dropdownItem = document.querySelectorAll(".dropdown-item");
+
+theme.onclick = function (){
+	if (jumbo.classList.contains("jumbotron")){
+		jumbo.classList.add("dark-jumbo");
+		jumbo.classList.remove("jumbotron");
+	} else {
+		jumbo.classList.add("jumbotron");
+		jumbo.classList.remove("dark-jumbo");
+	}
+	if (navbar.classList.contains("tentaclenav")){
+		navbar.classList.add("tentaclenav-dark");
+		navbar.classList.remove("tentaclenav");
+	} else {
+		navbar.classList.add("tentaclenav");
+		navbar.classList.remove("tentaclenav-dark");
+	}
+	if (!body.classList.contains("dark-body")){
+		body.classList.add("dark-body");
+	} else {
+		body.classList.remove("dark-body");
+	}
+	if (navbarTheme.classList.contains("navbar-light")) {
+		navbarTheme.classList.add("navbar-dark");
+		navbarTheme.classList.remove("navbar-light");
+	} else {
+		navbarTheme.classList.add("navbar-light");
+		navbarTheme.classList.remove("navbar-dark");
+	}
+	for (i = 0; i < dropdown.length; i++){
+		if (dropdown[i].classList.contains("tentacledrop")) {
+			dropdown[i].classList.add("tentacledrop-dark");
+			dropdown[i].classList.remove("tentacledrop");
+		} else {
+			dropdown[i].classList.add("tentacledrop");
+			dropdown[i].classList.remove("tentacledrop-dark");
+		}	
+	}
+	for (i = 0; i < dropdownItem.length; i++){
+		if (dropdownItem[i].classList.contains("dropdown-item")){
+			dropdownItem[i].classList.add("dropdown-item-dark");
+			dropdownItem[i].classList.remove("dropdown-item");
+		} else {
+			dropdownItem[i].classList.add("dropdown-item");
+			dropdownItem[i].classList.remove("dropdown-item-dark");
+		}
+	}
+}
