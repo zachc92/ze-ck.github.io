539ada9d2359968e0831c5f7ffbc1e7c76352826
Zachary <53413395+ze-ck@users.noreply.github.com>
2019-09-03 16:15:56 -0400

    Updated JS file
    Converted code to jQuery, cut length substantially

--- theme.js
+++ theme.js
@@ -1,75 +1,20 @@
 var theme = document.querySelector("#meteor");
-var navbar = document.querySelector(".tentaclenav-dark");
-var body = document.querySelector("body");
-var navbarTheme = document.querySelector(".navbar-dark");
-var dropdown = document.querySelectorAll(".tentacledrop-dark");
-var dropdownItem = document.querySelectorAll(".dropdown-item-dark");
-var hr = document.querySelector("hr");
-
-// setThemeFromCookie();
 
 theme.onclick = function(){
 	setTheme();
 }
 
 function setTheme(){
-	if (navbar.classList.contains("tentaclenav")){
-		navbar.classList.add("tentaclenav-dark");
-		navbar.classList.remove("tentaclenav");
-	} else {
-		navbar.classList.add("tentaclenav");
-		navbar.classList.remove("tentaclenav-dark");
-	}
-	if (body.classList.contains("light-body")){
-		body.classList.add("dark-body");
-		body.classList.remove("light-body");
-	} else {
-		body.classList.remove("dark-body");
-		body.classList.add("light-body");
-	}
-	if (navbarTheme.classList.contains("navbar-light")) {
-		navbarTheme.classList.add("navbar-dark");
-		navbarTheme.classList.remove("navbar-light");
-	} else {
-		navbarTheme.classList.add("navbar-light");
-		navbarTheme.classList.remove("navbar-dark");
-	}
-	for (i = 0; i < dropdown.length; i++){
-		if (dropdown[i].classList.contains("tentacledrop")) {
-			dropdown[i].classList.add("tentacledrop-dark");
-			dropdown[i].classList.remove("tentacledrop");
-		} else {
-			dropdown[i].classList.add("tentacledrop");
-			dropdown[i].classList.remove("tentacledrop-dark");
-		}	
-	}
-	for (i = 0; i < dropdownItem.length; i++){
-		if (dropdownItem[i].classList.contains("dropdown-item")){
-			dropdownItem[i].classList.add("dropdown-item-dark");
-			dropdownItem[i].classList.remove("dropdown-item");
-		} else {
-			dropdownItem[i].classList.add("dropdown-item");
-			dropdownItem[i].classList.remove("dropdown-item-dark");
-		}
-	}
-	if (hr.classList.contains("darkHr")){
-		hr.classList.add("lightHr");
-		hr.classList.remove("darkHr");
-	} else {
-		hr.classList.add("darkHr");
-		hr.classList.remove("lightHr");
-	}
+	$('nav').toggleClass("tentaclenav");
+	$('nav').toggleClass("tentaclenav-dark");
+	$('nav').toggleClass("navbar-light");
+	$('nav').toggleClass("navbar-dark");
+	$('body').toggleClass("light-body");
+	$('body').toggleClass("dark-body");
+	$('div .dropdown-menu').toggleClass("tentacledrop");
+	$('div .dropdown-menu').toggleClass("tentacledrop-dark");
+	$('div .dropdown-menu a').toggleClass("dropdown-item");
+	$('div .dropdown-menu a').toggleClass("dropdown-item-dark");
+	$('hr').toggleClass("lightHr");
+	$('hr').toggleClass("darkHr");
 }
-
-// function isDarkThemeSelected(){
-// 	return document.cookie.match(/darkTheme/i) != null;
-// }
-
-// function setThemeFromCookie(){
-// 	body.className = isDarkThemeSelected() ? "dark-body" : "light-body";
-// 	navbarTheme.className = isDarkThemeSelected() ? "navbar-dark navbar navbar-expand-lg" : "navbar-light navbar navbar-expand-lg";
-// 	dropdown.className = isDarkThemeSelected() ? "tentacledrop-dark dropdown-menu" : "tentacledrop dropdown-menu";
-// 	dropdownItem.className = isDarkThemeSelected() ? "dropdown-item-dark" : "dropdown-item";
-// 	jumbo.className = isDarkThemeSelected() ? "dark-jumbo" : "jumbotron";
-// 	navbar.className = isDarkThemeSelected() ? "tentaclenav-dark" : "tentaclenav";
-// }
